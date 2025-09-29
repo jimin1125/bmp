65fa35d2b5f03341891fc84d40a0a77e6daf2fc7
dorcus852 <117098385+dorcus852@users.noreply.github.com>
2025-09-29 23:47:14 +0900

    upload

--- server/index.js
+++ server/index.js
@@ -13,7 +13,11 @@ const pool = new Pool({
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+const path = require("path");
+app.use(express.static(path.join(__dirname, "..", "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
+});
 // 미들웨어
 app.use(express.json());
 app.use(cookieParser());
